a5f18232303a20b06bf3b2d2278e7b94b1554f27
Zdeněk Biberle <xbiber00@stud.fit.vutbr.cz>
2012-10-08 08:38:00 +0200

    Přidána prvotní verze evaluace AST

---
+++ unitests/ast/test.c
@@ -0,0 +1,62 @@
+#include "ast.h"
+#include "../test.h"
+
+BEGIN_TEST
+
+	Statement testStatements[] = {
+		{
+			.type = ASSIGNMENT,
+			.value = {
+				.assignment = {
+					.destination = 0,
+					.source = {
+						.type = CONSTANT,
+						.value = {
+							.constant = {
+								.type = typeNumeric,
+								.data = {
+									.numeric = 5.0
+								}
+							}
+						}
+					}
+				}
+			}
+		},
+		{
+			.type = RETURN,
+			.value = {
+				.ret = {
+					.type = VARIABLE,
+					.value = {
+						.variable = 0
+					}
+				}
+			}
+		}
+	};
+
+	Function f = {
+		.type = USER_DEFINED,
+		.value = {
+			.userDefined = {
+				.statements = testStatements,
+				.statementCount = 2,
+				.variableCount = 1
+			}
+		},
+		.paramCount = 1
+	};
+	
+	Value glob[1];
+	Value loc[1];
+	
+	Context ctx = { glob, loc };
+	
+	Value ret = evalFunction( &f, NULL, 0, &ctx );
+	
+	char* s = getValueString( &ret );
+	STRTEST( s, "5" );
+	free( s ); 
+
+END_TEST
