577113970237cc07168863d9061e25f15cb3b7bc
Jan Kalina <xkalin03@stud.fit.vutbr.cz>
2012-10-09 21:07:44 +0200

    Testy: oprava vylepseni zkracenych testu

--- unitests/tests.sh
+++ unitests/tests.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-echo $1
 FILES=`ls unitests`
 for FILE in $FILES; do
 	if [ -d "unitests/$FILE" ]; then
 		echo "*********************************"
 		echo "  Test: $FILE"
 		echo "*********************************"
-		if [ "$1" == "valgrind" ]; then
-			valgrind --leak-check=full -q unitests/$FILE/test > "unitests/$FILE/out" 2>&1
-		else
-			unitests/$FILE/test > "unitests/$FILE/out" 2>&1
-		fi
-		FILESIZE=$(stat -c%s "unitests/$FILE/out")
-		if [[ $FILESIZE > 1 ]] ; then # Nekdy soubor obsahoval \n
-			cat unitests/$FILE/out
-		else
-			echo "Valgrind korektni"
+		if [ "$1" != "valgrind" ]; then
 			unitests/$FILE/test
-	   	if [ "$?" == "0" ]; then
+		  	if [ "$?" == "0" ]; then
 				echo "Exit korektni"
+			fi
+		else
+			valgrind --leak-check=full -q unitests/$FILE/test > "unitests/$FILE/out" 2>&1
+			FILESIZE=$(stat -c%s "unitests/$FILE/out")
+			if [[ $FILESIZE > 1 ]] ; then # Nekdy soubor obsahoval \n
+				cat unitests/$FILE/out
+			else
+				echo "Valgrind korektni"
+				unitests/$FILE/test
+		  	 	if [ "$?" == "0" ]; then
+					echo "Exit korektni"
+				fi
 			fi
 		fi
 	fi
