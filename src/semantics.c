65e47acb38c8d6df14ffbffbbedea2cc08d71e24
Jan Kalina <xkalin03@stud.fit.vutbr.cz>
2012-10-16 20:30:51 +0200

    Semantics: AddToStatementList

--- src/semantics.c
+++ src/semantics.c
@@ -1,7 +1,42 @@
 #include "semantics.h"
 #include "syntax.h"
+#include "ast.h"
+
+static inline StatementList newStatementList(){
+	return (StatementList){ .item=NULL, .count=0 };
+}
+
+void AddToStatementList(StatementList *list, Statement new){
+	if(list->item==NULL){ // novy seznam
+		list->count=1;
+		list->item=malloc(sizeof(Statement));
+	}else{ // pridani do neprazdneho seznamu
+		list->count++;
+		list->item=realloc(list->item,list->count*sizeof(Statement));		
+	}
+	MALLCHECK(list->item);
+	memcpy(&new,&list->item[list->count-1],sizeof(Statement));
+}
 
 Function semantics(FILE *f){
-	syntax(f);
+	Token t;
+	StatementList list=newStatementList();
+	
+	
+	
+	t=syntax(f);
+	
+	AddToStatementList(&list, (Statement){.type=ASSIGNMENT});
+	
+	
+	
+	return (Function){
+		.type=USER_DEFINED,
+		.value.userDefined={
+			.statements=list,
+			.variableCount=0
+		},
+		.paramCount=0
+	};
 }
 
